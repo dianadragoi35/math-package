a072a0f54d1d3bf69997d374dc73a2e592086c02
Diana Dragoi <dianadragoi35@gmail.com>
2022-03-29 16:00:29 +0300

    function calcPrimeNumbers

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-// include_once('src/MathPackage.php'); 
+//include_once('src/MathPackage.php'); 
 // include_once('tests.php'); 
 
 // print_r("<pre>");
@@ -8,6 +8,11 @@
 // print_r("<br>");
 // print_r(MathPackage::calcPrimeNumbers( $paramCalcPrimeNumbers ));
 // print_r("<br>");
+
+// $paramCalcFactorial = 14;
 // print_r(MathPackage::calcFactorial( $paramCalcFactorial ));
+
+// print_r(MathPackage::to_xml([20, 31, 37, 41, 45, 43, 47, 53, 59, 90,'asd']));
+
 
 ?>
--- src/MathPackage.php
+++ src/MathPackage.php
@@ -13,14 +13,18 @@ class MathPackage
     public static function calcDivisors(int $num): array
     {
         $divisors = array();
-        for($i = 2; $i * $i < $num; $i ++) {
-            if ($num % $i == 0) {
-                $divisors[] = $i;
-                if($i != $num/$i){
-                    $divisors[] = $num/$i;
+        if(is_numeric($num))
+        {
+             for($i = 2; $i * $i < $num; $i ++) {
+                if ($num % $i == 0) {
+                    $divisors[] = $i;
+                    if($i != $num/$i){
+                        $divisors[] = $num/$i;
+                    }
                 }
             }
         }
+       
         return $divisors;
     }
 
@@ -31,16 +35,20 @@ class MathPackage
      * @return int
      */
     public static function calcFactorial(int $num): int
-    {
-        if($num >= 0 && $num <= 12){
-            $fact = 1;
-            for($i = $num; $i >= 1; $i--) {
-                $fact *= $i;
-            }
-            return $fact;
-        }else{
-            return "Wrong input.";
-        }	
+    {   
+        if(is_numeric($num))
+        {
+            if($num >= 0 && $num <= 12){
+                $fact = 1;
+                for($i = $num; $i >= 1; $i--) {
+                    $fact *= $i;
+                }
+                return $fact;
+            }else{
+                return 0;
+            }	
+        }
+        return 0;
     }
 
     /**
@@ -50,33 +58,74 @@ class MathPackage
      * @return bool
      */
     public static function isPrimeNumber(int $num): bool
-    {
-        if(count(MathPackage::calcDivisors($num))){
-            return false;
+    {   
+        if(is_numeric($num)){
+            if(count(MathPackage::calcDivisors($num))){
+                return false;
+            }
+            return true;
         }
-        return true;	
+        return false;
+        	
     }
 
      /**
-     * Check if a number is prime
+     * Takes the prime numbers from an array of integers
      * @param  array $nums
      *
-     * @return array
+     * @return array $primes
      */
-    public static function calcPrimeNumbers(array $nums): array
+    public static function filterPrimes(array $nums): array
     {
         $primes = array();
         foreach($nums as $num){
-            if(MathPackage::isPrimeNumber($num)){
-                $primes[] = $num;
-            }
+            if(is_numeric($num)){
+                if(MathPackage::isPrimeNumber($num)){
+                    $primes[] = $num;
+                }
+            }            
         }
         return $primes;	
     }
 
-    // public static function to_xml(SimpleXMLElement $object, array $data){
+     /**
+     * Takes an array with integers finds the prime numbers and returns the result as a XML document
+     * @param array $nums
+     * @param bool $saveXML, true for saving XML file, false for display XML
+     *
+     * @return result as XML document
+     */
+    public static function calcPrimeNumbers(array $nums, bool $saveXML = false){
 
-    // }
+        $nums = MathPackage::filterPrimes($nums);
+
+        $xml = new DOMDocument('1.0',"UTF-8");
+        $xml->preserveWhiteSpace = false;
+        $xml->formatOutput = true;
+
+        $primeNumbers   = $xml->createElement("primeNumbers");
+        $primeAttribute = $xml->createAttribute('amount');
+        $primeAttribute->value = "{".count($nums)."}";
+        $primeNumbers->appendChild($primeAttribute);
+        $xml->appendChild($primeNumbers);
+        
+        $result = $xml->createElement('result');
+        $primeNumbers->appendChild($result);
+
+        foreach ($nums as $key => $num) {
+            if(is_numeric($num)){
+                $number = $xml->createElement('number',"{".$num."}");
+                $result->appendChild($number);
+            }
+        }
+        
+        if($saveXML){
+            $xml->save('primeNumbers.xml');
+        }else{
+            return "<xmp>".$xml->saveXML()."</xmp>";
+        }
+
+    }
 
 
 }
